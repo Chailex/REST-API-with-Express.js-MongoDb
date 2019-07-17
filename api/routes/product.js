0e5acdd0b7ea525b467c442953c85f631d46b6b5
Chailex <32385484+Chailex@users.noreply.github.com>
2019-07-17 14:03:29 -0400

    Add files via upload

---
+++ api/routes/product.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
+
+router.get('/',(req,res,next)=>{
+	res.status(200).json({
+		message:'hello i am handling get request',
+
+	});
+});
+
+router.post('/',(req,res,next)=>{
+	const product = new Product({
+		_id: new mongoose.Types.ObjectId(),
+		name : req.body.name,
+		price : req.body.price
+	});
+	product
+	.save()
+	.then(result => {
+		console.log(result);
+		res.status(201).json({
+		message:'hello i am handling post request',
+		createdProduct : result,
+
+	});
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({
+			error:err
+		})
+	});
+	
+});
+
+router.get('/:productId',(req,res,next)=>{
+	const id = req.params.productId;
+	Product.findById(id)
+	.exec()
+	.then(doc => {
+		console.log(doc);
+		res.status(200).json(doc);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({error : err});
+	});
+})
+
+router.patch('/:product',(req,res,next)=>{
+	res.status(200).json({
+		message : 'updated product',
+	});
+})
+
+router.delete('/:product',(req,res,next)=>{
+	res.status(200).json({
+		message : "deleted product",
+	});
+})
+module.exports = router;
